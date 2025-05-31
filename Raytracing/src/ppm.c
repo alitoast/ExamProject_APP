bb5423ec9283c97edf1772a49cb0ee88407d92a8
alitoast <alice.macuz@gmail.com>
2025-05-31 23:53:44 +0200

    minor modification in printing messages

--- Raytracing/src/ppm.c
+++ Raytracing/src/ppm.c
@@ -15,7 +15,7 @@ bool save_ppm(const char *filename, unsigned char *image, int width, int height)
     // crea file se non esiste, sovrascrivi se esiste; con permessi di lettura e scrittura per tutti
     int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0666);
     if (fd < 0) {
-        perror("Errore: impossibile aprire il file per scrittura");
+        printf("Errore: impossibile aprire il file per scrittura");
         return false;
     }
 
@@ -26,7 +26,7 @@ bool save_ppm(const char *filename, unsigned char *image, int width, int height)
 
     // ridimensiona il file alla dimensione necessaria
     if (ftruncate(fd, file_size) == -1) {
-        perror("Errore: ridimensionamento fallito");
+        printf("Errore: ridimensionamento fallito");
         close(fd);
         return false;
     }
@@ -34,7 +34,7 @@ bool save_ppm(const char *filename, unsigned char *image, int width, int height)
     // mappa il file in memoria
     void *map = mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (map == MAP_FAILED) {
-        perror("Errore: mmap fallito");
+        printf("Errore: mmap fallito");
         close(fd);
         return false;
     }
@@ -45,12 +45,12 @@ bool save_ppm(const char *filename, unsigned char *image, int width, int height)
 
     // sincronizza le modifiche su disco
     if (msync(map, file_size, MS_SYNC) == -1) {
-        perror("Errore: sincronizzazione con disco fallita");
+        printf("Errore: sincronizzazione con disco fallita");
     }
 
     // libera la memoria e chiudi il file
     if (munmap(map, file_size) == -1) {
-        perror("Errore: munmap fallito");
+        printf("Errore: munmap fallito");
     }
     close(fd);
 
