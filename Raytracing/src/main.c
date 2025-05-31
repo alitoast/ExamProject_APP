fb8eaf412420651ecdf6ee1df22132e446f3fba5
alitoast <alice.macuz@gmail.com>
2025-05-31 17:15:53 +0200

    skeleton main.c

--- Raytracing/src/main.c
+++ Raytracing/src/main.c
@@ -3,6 +3,20 @@
 #include "scene/scene.h"
 
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        printf("Uso: %s <file scena> <output PPM> <larghezza> <altezza>\n", argv[0]);
+        return 1;
+    }
+
+    const char *scene_file = argv[1];
+    const char *output_file = argv[2];
+    int width = atoi(argv[3]);
+    int height = atoi(argv[4]);
+
+    Scene scene;
+    load_scene(scene_file, &scene);
+
+    free(scene.spheres);
     return 0;
 }
