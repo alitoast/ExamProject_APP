f2732a45c158ca7beba84a71d41a16324e50a390
alitoast <alice.macuz@gmail.com>
2025-05-31 16:54:42 +0200

    ppms

---
+++ Raytracing/src/ppm.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ppm.h"
+
+void save_ppm(const char *filename, unsigned char *image, int width, int height) {
+
+}
---
+++ Raytracing/src/ppm.h
@@ -0,0 +1,6 @@
+#ifndef PPM_H
+#define PPM_H
+
+void save_ppm(const char *filename, unsigned char *image, int width, int height);
+
+#endif
