e72202bf80851e6e13ccc8766017b2263e97344c
alitoast <alice.macuz@gmail.com>
2025-05-31 23:21:04 +0200

    updated ppm.h to reflect fixes in ppm.c

--- Raytracing/src/ppm.h
+++ Raytracing/src/ppm.h
@@ -6,6 +6,9 @@ SM3201385
 #ifndef PPM_H
 #define PPM_H
 
-void save_ppm(const char *filename, unsigned char *image, int width, int height);
+#include <stdbool.h>
+#include "scene.h"
+
+bool save_ppm(const char *filename, unsigned char *image, int width, int height);
 
 #endif
