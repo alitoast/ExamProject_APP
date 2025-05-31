899edcecccec2cbc7a6c05cfdae9c8643ad61cee
alitoast <alice.macuz@gmail.com>
2025-05-31 19:44:59 +0200

    save_ppm function added

--- Raytracing/src/ppm.c
+++ Raytracing/src/ppm.c
@@ -3,5 +3,13 @@
 #include "ppm.h"
 
 void save_ppm(const char *filename, unsigned char *image, int width, int height) {
+    FILE *file = fopen(filename, "wb");
+    if (!file) {
+        printf("Errore: impossibile scrivere %s\n", filename);
+        exit(1);
+    }
 
+    fprintf(file, "P6\n%d %d\n255\n", width, height);
+    fwrite(image, 3, width * height, file);
+    fclose(file);
 }
