18ec85a0a1b967363cfcaf095f542bc73cbff21f
alitoast <alice.macuz@gmail.com>
2025-06-01 19:26:21 +0200

    improved comments in ppm.c

--- Raytracing/src/ppm.c
+++ Raytracing/src/ppm.c
@@ -11,8 +11,21 @@ SM3201385
 #include <string.h>
 #include "ppm.h"
 
+
+/**
+ * Funzione: save_ppm
+ * Crea un file di output in formato binario PPM, mappa il file
+ * in memoria, scrive l'header e i dati dell'immagine in memoria condivisa, e
+ * infine sincronizza il contenuto su disco.
+ *
+ *
+ * @param filename Nome del file di output.
+ * @param image Puntatore a un array contenente i dati RGB (3 byte per pixel).
+ * @param width Larghezza dell'immagine in pixel.
+ * @param height Altezza dell'immagine in pixel.
+ * @return true se l'immagine è stata salvata correttamente, false in caso di errore.
+ */
 bool save_ppm(const char *filename, unsigned char *image, int width, int height) {
-    // crea file se non esiste, sovrascrivi se esiste; con permessi di lettura e scrittura per tutti
     int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0666);
     if (fd < 0) {
         printf("Errore: impossibile aprire il file per scrittura");
