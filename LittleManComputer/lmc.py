d3a31460f3b22e665467b3aaf3c30ce3b35409d9
alitoast <alice.macuz@gmail.com>
2025-06-03 18:32:56 +0200

    added load_program and execute methods

--- LittleManComputer/lmc.py
+++ LittleManComputer/lmc.py
@@ -1,4 +1,7 @@
-# Alice Macuz SM3201385
+#
+# Alice Macuz 
+# SM3201385
+#
 
 from copy import copy
 
@@ -11,3 +14,23 @@ class LMC:
         self.input_buffer = input_values or []
         self.output_buffer = []
         self.flag = 0  # 1 if overflow or underflow, 0 otherwise
+
+    def load_program(self, memory_image):
+        #
+        # carica codice macchina in memoria
+        #
+        if len(memory_image) > 100:
+            raise self.MemoryLimitError("Errore: Programma troppo grande per memoria.")
+        self.ram = copy(memory_image)
+
+        def execute(self, mode="n"):
+        #
+        # esegue tutto il programma oppure step-by-stepp
+        #
+        try:
+            while True:
+                self.execute_step(mode)
+        except StopIteration:
+            print("Execution finished.")
+
+    
