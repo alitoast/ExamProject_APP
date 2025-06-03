3873270aff909370a66ab832d44704fe88f66443
alitoast <alice.macuz@gmail.com>
2025-06-03 17:31:17 +0200

    add assembler class skeleton

---
+++ LittleManComputer/assembler.py
@@ -0,0 +1,14 @@
+#
+# Alice Macuz 
+# SM3201385
+#
+
+class Assembler:
+    def __init__(self):
+        self.opcodes ={}
+        self.labels = {}
+        self.memory = [0] * 100
+    
+    def assemble(self, source_code: str):
+        return self.memory
+    
